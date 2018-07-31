83ed51ffecfce958555a5438b75c21be7502855c
AndreasMN <andycutter@godmail.dk>
2018-07-31 14:21:45 +0200

    write scenario for creating new task add snippets to step-files

---
+++ features/step_definitions/assertion_steps.rb
@@ -0,0 +1,27 @@
+When("I am at the index page") do
+    visit '/'
+end
+
+When("I click on {string} link") do |string|
+pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("I should be on {string} page") do |string|
+pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("I fill in {string} with {string}") do |string, string2|
+pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("I click on {string}") do |string|
+pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("there should be a New Task in the database") do
+pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("I should see {string}") do |string|
+pending # Write code here that turns the phrase above into concrete actions
+end
---
+++ features/visitor_can_create_new_task.feature
@@ -0,0 +1,16 @@
+Feature: A visitor can create new task
+    As a visitor,
+    In order to get my furniture refurbished
+    I would to be able create a new task
+
+    Scenario: Create task
+        When I am at the index page
+        And I click on "New Task" link
+        Then I should be on "New Task" page
+        And I fill in "Title" with "My Task"
+        And I fill in 'Description' with 'Really old table that needs refurnishing'
+        And I fill in 'Budget' with '100'
+        And I fill in 'Location' with 'Stockholm'
+        And I click on 'Post Task'
+        Then there should be a New Task in the database
+        And I should see 'Task successfully created'
