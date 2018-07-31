d03f692329644432a1de7ed4e6565332df84cb9d
AndreasMN <andycutter@godmail.dk>
2018-07-31 15:54:30 +0200

    remove ambigious match from steps and typo in new.html.haml

--- features/step_definitions/assertion_steps.rb
+++ features/step_definitions/assertion_steps.rb
@@ -2,10 +2,6 @@ When("I am at the index page") do
     visit '/'
 end
 
-Then("there should be a New Task in the database") do
-pending # Write code here that turns the phrase above into concrete actions
-end
-
 Then("I should see {string}") do |string|
 pending # Write code here that turns the phrase above into concrete actions
 end
