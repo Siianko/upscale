13697c2aa84b1e4894018d04ffcc37186d9c92b2
AndreasMN <andycutter@godmail.dk>
2018-08-01 12:14:26 +0200

    remove unfinished rspec test to avoid errors

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,4 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  describe 'DB table' do
-    it { is_expected.to have_db_coloumn :id }
-    it { is_expected.to have_db_coloumn :id }
 end
